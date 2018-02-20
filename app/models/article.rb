ccfe21a8648e07b41f7d70800e2e4d2048bea9d4
gloriacruz <ally.cruz@gmail.com>
2018-02-19 18:46:48 -0800

    Delete article with comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article
   include Mongoid::Document
   field :title, type: String
   field :text, type: String
-  embeds_many :comments
+  embeds_many :comments, cascade_callbacks: true
   validates :title, presence: true
 end
